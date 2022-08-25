c097c017e8b79b188f2d1c43ab07aae513a05ebb
jerem_Ablrd <jeremy97426@gmail.com>
2022-08-25 13:50:47 +0400

    New version Elect_keyFigure, add function and read file.

--- Energy_Balance2021/PieChart/Elect_KeyFigure.py
+++ Energy_Balance2021/PieChart/Elect_KeyFigure.py
@@ -1,72 +1,62 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import numpy as np
+import numpy as nd
 import pandas as pd
 import seaborn as sns
 
+# -------  CHART FOR KEY FIGURE ELECTRICITY ------- #
 
-# Appel du Fichier
+# Read .xlsx (excel file) ; sheet_name = "Electricity Stat-2021"
 path = "C:/Users/jerem/PycharmProjects/pythonProject/Infography/Energy_Balance2021/Seychelles Energy Balance For 2021 - ver2 2.xlsx"
-file = pd.read_excel(path, sheet_name="Energy Balance-2021", header=2, index_col=[0, 14])
-df = file.iloc[1:36, 1:14]
+file = pd.read_excel(path, sheet_name="Electricity Stat-2021", header=39)
 
+# create Dataframe "df" for the table concerned [ELECTRICITY GENERATION MIX IN PUC]
+df = file.iloc[0:5, 0:3]
+print(df)
 
-# Pretraitement -> où tirer les data, traiter les data avant config
+# Generate data parameters
+energy = df['Technology'].iloc[0:4]
+value_energy = df['GWh'].iloc[0:4]
+share = df["Share"].iloc[0:4]
+total_energy = file.iloc[4:5, 1:2]
 
-# Générer figure
+# Change share values to percent
+share_percent = []
+for i in share.values:
+    share_percent.append(round(i*100, 1))
+share_percent = nd.array(share_percent)
 
-# "   "   data
+# Config chart
+Colors_share = ['Teal', '#42a1a1', '#ffde59', '#ff8c00']
+explode = (0, 0, 0.1, 0.1)
+center = (1.2, 1.2)
 
-# Show
-
-'''#generate figure
+# Generate figure
 plt.figure(figsize=(12, 9))
 
-#generate dataframe for pie plot
-Energies = 'Diesel - Heavy Fuel Oil', 'Diesel - Lite Fuel Oil', 'Solar PV', 'Wind Farm'
-Electricity_Generation = [86.7, 10.41, 1.35, 1.54]
-Colors = ['Teal', '#42a1a1', '#ffde59', '#ff8c00']
-explode = (0, 0, 0.3, 0.3)
-#generate graphique
-
-#plt.plot(x,y)
+# Generate Pie Chart
 plt.axis([0, 2.4, 0, 2.4])
-plt.pie(x= Electricity_Generation, labels= Energies, labeldistance=None, colors= Colors,
-        autopct=None, shadow=False, startangle=-0, counterclock=False, frame=False, center=(1.2,1.2), explode=explode)
+plt.pie(x=value_energy, labels=energy, labeldistance=None, colors=Colors_share,
+        autopct=None, shadow=False, startangle=-0, counterclock=False, frame=False,
+        center=center, explode=explode)
 
-#ajoue de text
-plt.text(0.7,1.2,"86.7%",
-         fontsize=30,
-         color="black",
-         verticalalignment ='top',
-         horizontalalignment ='center',
-         bbox =None
-        )
-plt.text(1.8,1.5,"10.4%",
-         fontsize=28,
-         color="black",
-         verticalalignment ='top',
-         horizontalalignment ='center',
-         bbox =None
-        )
-plt.text(2.70,1.30,"1.4%",
-         fontsize=25,
-         color="black",
-         verticalalignment ='top',
-         horizontalalignment ='center',
-         bbox =None
-        )
-plt.text(2.70,1.48,"1.5%",
-         fontsize=25,
-         color="black",
-         verticalalignment ='top',
-         horizontalalignment ='center',
-         bbox =None
-        )
+
+## ------- Print ------- ##
+def print_text(x: float, y: float, txt: str, fontsize: int, color: str, verti_align: str, horiz_align: str, bbox=None):
+    plt.text(x, y, txt, fontsize=fontsize, color=color, verticalalignment =verti_align,
+             horizontalalignment =horiz_align,)
+
+
+# Call function print_text()
+print_text(0.7, 1.2, str(share_percent[0]) + "%", 30, "black", 'top', 'center')
+print_text(1.8, 1.55, str(share_percent[1]) + "%", 28, "black", 'top', 'center')
+print_text(2.70, 1.30, str(share_percent[2]) + "%", 25, "black", 'top', 'center')
+print_text(2.70, 1.48, str(share_percent[3]) + "%", 25, "black", 'top', 'center',)
+
+plt.savefig('Electgene2020.png' ,transparent=True, dpi=300)
+plt.show()
 
 
 #plt.title('Store Inventory')
 #plt.ylabel('Product')
 #plt.xlabel('Quantity')
-plt.savefig('Electgene2020.png' ,transparent=True, dpi=300)
-plt.show()'''
