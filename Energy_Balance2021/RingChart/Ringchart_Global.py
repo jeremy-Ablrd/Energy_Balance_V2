de15e2a216f27b991794a0ecb23c071d63766bb3
jerem_Ablrd <jeremy97426@gmail.com>
2022-09-07 16:16:31 +0400

    modification of ring chart (en cour...) / modif of data for print better charts.

--- Energy_Balance2021/RingChart/Ringchart_Global.py
+++ Energy_Balance2021/RingChart/Ringchart_Global.py
@@ -12,6 +12,8 @@ import csv
 # plt.show() at the end
 
 # Call file
+from pandas import DataFrame
+
 path = "C:/Users/jerem/Desktop/Energy_Balance_V2/Energy_Balance2021/Seychelles Energy Balance For 2021 - ver2 2.xlsx"
 file = pd.read_excel(path, sheet_name="Energy Balance-2021", header=2, index_col=0)
 
@@ -19,7 +21,6 @@ file = pd.read_excel(path, sheet_name="Energy Balance-2021", header=2, index_col
 df = file.iloc[25:36, 0:13]
 rename_columns = df.rename(columns={'Prod. (+)                 Cons. (-)': 'Electricity',
                                     'Prod. (+)                 Cons. (-).1': 'Solar Water Heater'}, inplace=True)
-#print(df)
 
 
 def create_data_sector(sectors: list,):
@@ -35,36 +36,82 @@ def create_data_sector(sectors: list,):
         for values in series:
             list_.append(values)
         index_series.append(index_)
-        list_series.append(list_)
-
+        list_series.append(value_percent(list_))
     return index_series, list_series
 
+# change the data in percentage
+def value_percent(shares: list):
+    share_percent = []
+    for each_share in shares:
+        operation = (each_share / sum(shares)) * 100
+        share_percent.append(round(operation, 2))
+    return share_percent
 
-SECTORS = ['Residential', 'Service', 'Industry']
+
+SECTORS = ['Residential',
+           # 'Service', 'Industry'
+           ]
 index_global, energy_global = create_data_sector(SECTORS)        # call of function
 print("Index Global:", index_global)
 print("Energy Sector:", energy_global)
 colors = ['#52A8A8', '#A3B825', '#ffc966']
 
 # Create figure
-fig = plt.figure(figsize=(9, 12))
+
 
 # create data
-def chart(x, label, color):
+def chart(x, label, color=None):
     plt.figure(figsize=(12, 9))
     plt.pie(x=x, labels=label, colors=color, labeldistance=None,
             # explode=explode2,
-            pctdistance=0.85,
-            autopct='%1.0f%%', shadow=False, startangle=-0, counterclock=False, frame=False,
+            pctdistance=0.65,
+            autopct='%2.0f%%', shadow=False, startangle=-0, counterclock=False, frame=False,
             wedgeprops={'linewidth': 1, 'edgecolor': 'white'},
             textprops={'fontsize': 27, 'color': 'black'}
             # center=(1.2,1.2)
             )
     plt.show()
 
-    #modifier chart avec boucle for pour obtenir energy et label separement
-chart(energy_global, index_global, colors)
-for i in range(0, len(index_global)):
-    label_residential = index_global[i]
-    print("Sector", i+1, ':', str(label_residential))
 
+def simplified_dataframe(dataframe: DataFrame):
+    dict_ = {}
+    for i in dataframe.index:
+        if i == "Solar Water Heater":
+            dict_[i] = dataframe.loc[i]
+            dataframe.drop(axis=0, labels=i, inplace=True)
+        elif i == "Fuelwood & charcoal":
+            dict_[i] = dataframe.loc[i]
+            dataframe.drop(axis=0, labels=i, inplace=True)
+        elif i == "Kerosene":
+            dict_[i] = dataframe.loc[i]
+            dataframe.drop(axis=0, labels=i, inplace=True)
+    dict_df = pd.DataFrame(dict_).T
+    somme = dict_df.iloc[:].sum(axis=0)
+    # # print("Somme dictionnaire:", somme)
+    # # new DataFrame
+    new_df = pd.concat([dataframe, somme], axis=0)
+    print(new_df)
+    print(dict_df)
+    print(somme)
+   # return new_df
+
+
+
+
+# call data in for loop
+for energy, label in enumerate(index_global):
+    en_sector = energy_global[energy]
+    # chart(en_sector, label)
+    n_df = pd.DataFrame(en_sector, columns=['Value'], index=label)
+    sorted_n_df = n_df.sort_values(by='Value', ascending=False)
+    print(sorted_n_df)
+    simplified_dataframe(sorted_n_df)
+
+
+
+
+
+
+#chart(energy_global, index_global)
+
+    #modifier chart avec boucle for pour obtenir energy et label separement
